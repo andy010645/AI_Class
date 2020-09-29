71681123b2ed9b59d6ebcca9f6ac4ee2357b40a2
andy010645 <69691891+andy010645@users.noreply.github.com>
2020-09-29 22:56:51 +0800

    OK

--- Simulated_Annealing/main.py
+++ Simulated_Annealing/main.py
@@ -1,22 +1,55 @@
 from functions import func
-f=open('input.txt','r')
-lines=f.readlines()
-#lines=lines.replace(","," ")
-lines[0]=lines[0].replace(","," ")
-lines[0]=lines[0].replace("\0"," ")
-print(int(lines[0]))
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
+f=open('input.txt','r')
+x=f.readline().split(",")
+y=f.readline().split(",")
 def Brute_force():
     max=10000
-    for a in range():
-        for b in range(0,1):
-            print(b)
+    for a in range(int(x[0]),int(x[1])):
+        for b in range(int(y[0]),int(y[1])):
             if (func(a,b)<max):
                 max=func(a,b)
                 ans_a=a
-                ans_b=b
-        print(a)
-    
+                ans_b=b   
     return ans_a,ans_b,round(func(ans_a,ans_b),4)
-#def SA():    
-print(Brute_force())
+def Simulated_Annealing(a_search_space,b_search_space, func, t):
+    scale=np.sqrt(t)
+    a_init=np.random.choice(a_search_space)
+    b_init=np.random.choice(b_search_space)
+    a=a_init
+    b=b_init
+    cur=func(a,b)
+    a_history=[a]
+    b_history=[b]
+    iterations=2000
+    for i in range(iterations):
+        prop_a=int(a+np.random.normal()*scale)
+        prop_b=int(b+np.random.normal()*scale)
+        if (prop_a >=-60 and prop_a<=60 and prop_b>=-30 and prop_b<=70):
+            new_out=func(prop_a,prop_b)
+            diff=new_out-cur
+            if diff<0 or np.exp(-diff/(k*t))>np.random.rand():
+                #time.sleep(1)
+                a=prop_a
+                b=prop_b
+                cur=func(a,b)
+                a_history.append(a)
+                b_history.append(b)
+        t=0.9*t
+
+
+    return a,b,round(func(a,b),4),len(a_history)
+    
+
+
+#print(Brute_force())
+a_area=np.linspace(int(x[0]),int(x[1]),int(x[1])-int(x[0])+1)
+b_area=np.linspace(int(y[0]),int(y[1]),int(y[1])-int(y[0])+1)
+k=1
+print(Simulated_Annealing(a_area,b_area,func,100))
+
+
+
